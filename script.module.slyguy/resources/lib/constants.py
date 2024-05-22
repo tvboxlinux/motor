cc93b0560f7823eae685c391d718bb877a3cf3d7
Alejandro <40009875+tvboxlinux@users.noreply.github.com>
2024-05-21 20:09:09 -0400

    ok
    ok

--- script.module.slyguy/resources/lib/constants.py
+++
@@ -1,8 +0,0 @@
-from slyguy.constants import REPO_DOMAIN
-
-## NEWS ##
-NEWS_URL = REPO_DOMAIN+'/.repo/news.json.gz'
-ADDONS_URL = REPO_DOMAIN+'/.repo/addons.json.gz'
-ADDONS_MD5 = REPO_DOMAIN+'/.repo/addons.xml.md5'
-NEWS_CHECK_TIME = 3600 #1 Hour
-UPDATES_CHECK_TIME = 3600 #1 Hour
